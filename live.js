81803296e9a18328a66a41d74d5d715bc3942786
Mario <126658300+manogamika@users.noreply.github.com>
2023-11-21 18:37:15 +0700

    Create live.js

---
+++ live.js
@@ -0,0 +1,35 @@
+function sendMessage() {
+    var messageInput = document.getElementById("message-input");
+    var message = messageInput.value.trim();
+
+    if (message !== "") {
+  
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "sendMessage.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                messageInput.value = "";
+            }
+        };
+        xhr.send("message=" + encodeURIComponent(message));
+    }
+}
+function updateChat() {
+    var chatMessages = document.getElementById("chat-messages");
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "getMessages.php", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+
+            chatMessages.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+
+
+    setTimeout(updateChat, 1000);
+}
+updateChat();
